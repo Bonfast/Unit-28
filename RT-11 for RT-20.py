6654e9ff0d1a23981a31320983a23fdfbbf9cccc
Aleksandr <127786998+Bonfast@users.noreply.github.com>
2023-09-02 15:41:29 +0300

    Update RT-11 for RT-20.py

--- RT-11 for RT-20.py
+++ RT-11 for RT-20.py
@@ -1,29 +1,103 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+
 def test_case_11():
+    driver.get("https://b2c.passport.rt.ru")
+    agreement_link = driver.find_element_by_link_text("пользовательского соглашения")
+    agreement_link.click()
+
+test_case_11()
+driver.quit()
+        
+-----------------------------------------------------------------------------------------------
+
+from selenium import webdriver
+driver = webdriver.Chrome()
+
+def test_case_12():
     driver.get("https://b2c.passport.rt.ru")
     registration_button = driver.find_element_by_link_text("Зарегистрироваться")
     registration_button.click()
-    
-    # Ввод имени
+   
     first_name_input = driver.find_element_by_name("firstName")
     first_name_input.send_keys("Имя")
-    
-    # Ввод фамилии
+
     last_name_input = driver.find_element_by_name("lastName")
     last_name_input.send_keys("Фамилия")
     
-    # Ввод номера мобильного телефона для регистрации
     mobile_input = driver.find_element_by_id("address")
     mobile_input.send_keys("номер_телефона")
-    
-    # Ввод пароля для регистрации
+
     password_input = driver.find_element_by_id("password")
     password_input.send_keys("пароль")
     
-    # Ввод подтверждения пароля
     password_confirm_input = driver.find_element_by_id("password-confirm")
     password_confirm_input.send_keys("пароль")
     
-    # Нажатие на кнопку "Зарегистрироваться"
     register_button = driver.find_element_by_xpath("//button[text()='Зарегистрироваться']")
     register_button.click()
 
+test_case_12()
+driver.quit()
+
+-----------------------------------------------------------------------------------------------
+
+from selenium import webdriver
+driver = webdriver.Chrome()
+
+def test_case_13():
+    driver.get("https://b2c.passport.rt.ru")
+    registration_button = driver.find_element_by_link_text("Зарегистрироваться")
+    registration_button.click()
+   
+    first_name_input = driver.find_element_by_name("firstName")
+    first_name_input.send_keys("Имя")
+
+    last_name_input = driver.find_element_by_name("lastName")
+    last_name_input.send_keys("Фамилия")
+    
+    mobile_input = driver.find_element_by_id("address")
+    mobile_input.send_keys("уже_зарегестрированный_адрес_электронной_почты")
+
+    password_input = driver.find_element_by_id("password")
+    password_input.send_keys("пароль")
+    
+    password_confirm_input = driver.find_element_by_id("password-confirm")
+    password_confirm_input.send_keys("пароль")
+    
+    register_button = driver.find_element_by_xpath("//button[text()='Зарегистрироваться']")
+    register_button.click()
+
+test_case_13()
+driver.quit()
+
+-----------------------------------------------------------------------------------------------
+
+from selenium import webdriver
+driver = webdriver.Chrome()
+
+def test_case_14():
+    driver.get("https://b2c.passport.rt.ru")
+    registration_button = driver.find_element_by_link_text("Зарегистрироваться")
+    registration_button.click()
+   
+    first_name_input = driver.find_element_by_name("firstName")
+    first_name_input.send_keys("ввод_цифр_в_поле_имя")
+
+    last_name_input = driver.find_element_by_name("lastName")
+    last_name_input.send_keys("ввод_цифр_в_поле_фамилия")
+    
+    mobile_input = driver.find_element_by_id("address")
+    mobile_input.send_keys("номер_телефона")
+
+    password_input = driver.find_element_by_id("password")
+    password_input.send_keys("пароль")
+    
+    password_confirm_input = driver.find_element_by_id("password-confirm")
+    password_confirm_input.send_keys("пароль")
+    
+    register_button = driver.find_element_by_xpath("//button[text()='Зарегистрироваться']")
+    register_button.click()
+
+test_case_14()
+driver.quit()
